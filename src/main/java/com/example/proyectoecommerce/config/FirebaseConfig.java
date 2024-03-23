3e67562454dd34242f325c506571bf7bd4f6ae51
vstrom650xt <cerveramonzo@gmail.com>
2024-03-23 13:19:49 +0100

    CAMBIOS EN EL CONFIG

--- src/main/java/com/example/proyectoecommerce/config/FirebaseConfig.java
+++ src/main/java/com/example/proyectoecommerce/config/FirebaseConfig.java
@@ -12,25 +12,37 @@ import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
+    private static FirebaseApp firebaseApp;
+
     @Bean
     public FirebaseApp initialization(){
-        System.out.println("Firebase inicializado correctamente.");
+        if (firebaseApp == null) {
+            System.out.println("Firebase inicializado correctamente.");
 
-        FileInputStream serviceAccount = null;
-        try {
-            serviceAccount = new FileInputStream("./ecommerce-176bc-firebase-adminsdk-q14vx-1b03aed55e.json");
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+            FileInputStream serviceAccount = null;
+            try {
+                serviceAccount = new FileInputStream("./ecommerce-176bc-firebase-adminsdk-q14vx-1b03aed55e.json");
 
-        try {
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .setDatabaseUrl("https://ecommerce-176bc-default-rtdb.europe-west1.firebasedatabase.app")
-                    .build();
-            return FirebaseApp.initializeApp(options);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+                FirebaseOptions options = new FirebaseOptions.Builder()
+                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                        .setDatabaseUrl("https://ecommerce-176bc-default-rtdb.europe-west1.firebasedatabase.app")
+                        .build();
+                firebaseApp = FirebaseApp.initializeApp(options, "DEFAULT");
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } finally {
+                if (serviceAccount != null) {
+                    try {
+                        serviceAccount.close();
+                    } catch (IOException e) {
+                        // Log the exception or handle it as needed
+                        e.printStackTrace();
+                    }
+                }
+            }
         }
+        return firebaseApp;
     }
 }
