b510309a970e64940fa97dc462822af8304b8102
vstrom650xt <cerveramonzo@gmail.com>
2024-03-23 13:24:08 +0100

    borrado del pom dependencia JPA

--- src/main/java/com/example/proyectoecommerce/ProyectoEcommerceApplication.java
+++ src/main/java/com/example/proyectoecommerce/ProyectoEcommerceApplication.java
@@ -7,11 +7,10 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import java.io.File;
 import java.util.Objects;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})public class ProyectoEcommerceApplication {
+@SpringBootApplication()
+public class ProyectoEcommerceApplication {
 
     public static void main(String[] args) {
-//        ClassLoader classLoader = ProyectoEcommerceApplication.class.getClassLoader();
-//        File file = new File(Objects.requireNonNull(classLoader.getResource("ecommerce-176bc-firebase-adminsdk-q14vx-1b03aed55e.json")).getParentFile());
         SpringApplication.run(ProyectoEcommerceApplication.class, args);
     }
 
